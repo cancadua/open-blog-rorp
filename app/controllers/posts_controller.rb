da1595ee5d19aa2f621e8781bc1fc8e4fff720fe
Adrian <cancadua@protonmail.com>
2022-12-10 11:48:50 +0100

    Cleaned code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,8 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
 
-  # GET /posts or /posts.json
   def index
     @posts = Post.all
-  end
-
-  # GET /posts/1 or /posts/1.json
-  def show
   end
 
   # GET /posts/new
@@ -15,35 +10,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  # GET /posts/1/edit
-  def edit
-  end
-
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
 
     respond_to do |format|
-      if @post.save
-        format.html { redirect_to posts_path }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+      @post.save
+      format.html { redirect_to posts_path }
     end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
     respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to posts_path}
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+      @post.update(post_params)
+      format.html { redirect_to posts_path}
     end
   end
 
@@ -56,7 +37,6 @@ class PostsController < ApplicationController
     @post.destroy
     respond_to do |format|
       format.html { redirect_to posts_path}
-      format.json { head :no_content }
     end
   end
 
